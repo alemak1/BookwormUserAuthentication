a508c6abf1c3ca5f52c46ebb9c37ceb8a9926dcc
yongrenwang <czie.alexmak@gmail.com>
2017-02-16 13:39:23 +0800

    Implemented the pre method on the User schema in model.js so that the user password is first hashed (with salt), and so that the hashed password is stored in the document

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -25,6 +26,20 @@ var UserSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	}
+
+});
+
+//hash password before saving to database
+UserSchema.pre('save',function(next){
+	var user = this;
+	bcrypt.hash(user.password,10,function(err,hash){
+		if(err){
+			return next(err);
+		}
+
+		user.password = hash;
+		next();
+	})
 
 });
 
